1f35ef502513501294101203ca9210985f69cddb
tonytomo <colonelmacmillan@gmail.com>
2021-12-16 15:55:58 +0700

    Menyelesaikan rsa

--- UI/controllers/rsaController.py
+++ UI/controllers/rsaController.py
@@ -1,7 +1,7 @@
 from UI import app
 from flask import render_template, request
 
-from rsa import generate_private_key, generate_public_key
+from rsa import decrypt_data, encrypt_data, generate_private_key, generate_public_key
 
 @app.route('/rsa/generate-key', methods=['POST'])
 def generateKey():
@@ -26,3 +26,43 @@ def generateKey():
 
         return data
     
+@app.route('/rsa/encrypt', methods=['POST'])
+def encrypt():
+    request_data = request.get_json()
+    res = {
+        "encrypted": {}
+    }
+    if request.method == "POST":
+        block_length = 3
+        print(request_data)
+
+        public_key = [int(request_data['n']), int(request_data['e'])]
+
+        message = request_data['plain']
+        
+        res["encrypted"] = encrypt_data(message, public_key[0], public_key[1], block_length)
+        print(res)
+
+        return res
+
+@app.route('/rsa/decrypt', methods=['POST'])
+def decrypt():
+    request_data = request.get_json()
+    res = {
+        "decrypted": {}
+    }
+    if request.method == "POST":
+        block_length = 3
+        print(request_data)
+
+        public_key = [int(request_data['n']), int(request_data['e'])]
+        private_key = int(request_data['private_key'])
+
+        message = request_data['encrypted'].split()
+        map_message = map(int, message)
+        list_message = list(map_message)
+        
+        res["decrypted"] = decrypt_data(list_message, private_key, public_key[0], block_length)
+        print(res)
+
+        return res
